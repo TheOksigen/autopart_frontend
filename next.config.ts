6c908ff1df3e8201120090e3ecd93f39258cf4e1
https.oksigen <david@davidhtml.xyz>
2024-12-14 22:18:20 +0400

    fix img bug

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["parcabul.com.tr"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "parcabul.com.tr",
+      }
+    ]   
   }
 };
 
